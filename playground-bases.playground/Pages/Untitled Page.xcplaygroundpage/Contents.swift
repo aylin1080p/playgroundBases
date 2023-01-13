3c5502a407185cdf01abc0f11e24560cf6a70d3c
aylin güneş <aylngns94@gmail.com>
2023-01-13 22:05:26 +0100

    Set functions

--- playground-bases.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ playground-bases.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,6 +1,8 @@
  import UIKit
 
 
+ 
+
  var a:Int32 = 50
 if a < 70 {
     print("evet")
@@ -212,6 +214,121 @@ for i in isimler1{
     }
     
 }
+ 
+ 
+
+// set çalışması---collections -- aynı elemanı bir daha eklemez ve rastgelelik söz konusu--java-hashset--soru şıkları yer değiştirme örneği
+// array but it dosent have index
+
+let setIsmı = Set <Int>() // boş set tanımlama
+var setIsmı2: Set = ["bir","takım","stringler"]
+let setIsmı3: Set<String> = ["let","yada","set","kullanabilirsin"]
+
+//set işlemleri
+//1- append yerine insert kullanılı çünkü append sona eklerdi arraylerde ama burda setlerde insert eder araya bir yere veriyi ekleriz.
+// eklenen veriler forla alınır index numarası yoktur.
+let sehirlerSeti:Set = ["istanbul","adanamerkez","patlıyorherkes"]
+var denemeSet:Set<Float> = [1.2 , 0.3]
+let zetIsmı2:Set = [10 , 11 , 12]
+// aynı türden verileri kayıt etmez senin listeye eklemen bir şeyi değiştirmez
+
+var meyveler:Set = ["çilek", "muz", "elmaz","kivi"]
+
+for valuee in meyveler {
+    print(valuee)
+}
+ // her çalıştırmanda farklı bir sırayla print eder. Veriler arasında her sayfa yenilemede ya da her kullanıcı için farklı sıralamalar sağlayabilriisn.
+
+// kendince bir index numarası tutar onu da ilk yazdırdığı elemana 0 gelecek iekilde yerleştirir ama bir sonraki çalıştırmada aynı eleman farklı bir index içinde yer alıyor olabilr.
+
+for (indeks, meyve) in meyveler.enumerated(){
+    
+    print("\(indeks) \(meyve)")
+}
+
+//genelde indexle ilgili işlemler yapılmaz Set'lerde
+// eleman ekleme-- torbaya ekleme gibi düşün
+
+meyveler.insert("karpuz")
+
+for _ in 1...5{
+    
+    
+    
+    for j in meyveler {
+        if(j == "karpuz"){
+            print("burada zaten karpuz var")
+            break
+        }
+        else {
+            print("şimdilik karpuz yok")
+        }
+
+    }
+    
+    
+    
+    
+    // döngü içinde eleman eklenecek
+    // birden fazla eklenmicek ama döngü en az beş kez devam etsin dersek
+    
+}
+
+
+
+
+
+
+
+// hazır methotlar
+
+meyveler.isEmpty // true ya da false döndürecek dolu mu boş mu bakar boş ise true verir
+
+meyveler.count // Int çevirir kaç eleman varsa set içinde onu yazar
+
+meyveler.first // ilk elemanı yazdırır değer stringe onu sayıysa onu döndürür-- last yok
+
+meyveler.contains("muz") // içinde muz var mı yok mu kontrol eder saçma döngülere sokmana gerek kalmaz-- true ya da false döndürür
+
+meyveler.max() // max ya da min değerleri bulunur sayısal olarak
+meyveler.min()
+meyveler.removeAll() // hepsini temizler
+
+// set yapısına özgü methodlar
+
+
+let tekSayilar: Set = [1,3,5,7,9]
+
+var ciftSayilar: Set = [0,2,4,6,8]
+
+var asalSayilar: Set = [2,3,5,7]
+
+tekSayilar.union(ciftSayilar.sorted()) // her iki kümeyi de birleştirir benzer değerler zaten set'lerde ikinci kez tekrar etmezdi ama bir kez yazılır:
+// [0,1,2,3......9]
+
+tekSayilar.intersection(ciftSayilar).sorted() // her iki küöenin kesişimlerinden yeni bir küme oluşturur tekler ve çiftler asla kesişmez bu set boş olacaktır.
+
+tekSayilar.subtracting(asalSayilar).sorted() // tek sayılar kümseinden asal sayılar kümesini çıkarar kaln kümeden yeni bir set yaratır
+// 1 ve 9 kalır çünkü asaldaki 3,5,7 çıkarılacak
+
+tekSayilar.symmetricDifference(asalSayilar).sorted() // her iki kümeyi birleştirir kesişiminde kalan elemanları çıkartır hiç yazdırmaz yeni sete
+// 1,2,9 kalacak elimizde
+
+
+
+
+// dictionaries---- last collection -key-value
+
+var dic1 = [Int:String]() // int key'li String Value'li bir dictionary oluşturur ama boş olacak
+
+var dic2 = [3.14:"pi", 2.70:"iki yetmiş"] // tür tip belirtmeden
+
+let dic3:[Int:String] = [1:"Bir", 2:"iki"]
+
+
+
+
+
 
 
 
